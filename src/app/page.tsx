c080fedc5a7d182c0c8e3b9b873b30aa0c6a743c
ridhoakbarsyah <ridhoakbarsyah15@gmail.com>
2024-03-21 12:10:04 +0700

    Switch Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-// export default function Home() {
-//   return (
-//     <div className="text-center">
-//       <h1 className="title text-2xl mb-10 mt-10">Home</h1>
-//     </div>
-//   );
-// }
-
 import ContactTable from "@/components/contact-table";
 import Search from "@/components/search";
 import { CreateButton } from "@/components/buttons";
